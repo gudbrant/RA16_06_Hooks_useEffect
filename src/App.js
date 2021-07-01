98cb929648ad1139468584ac36fd82beb66a320b
gudbrant <61345734+gudbrant@users.noreply.github.com>
2021-07-01 11:49:17 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import List from './components/List';
+import Details from './components/Details';
+import { useState } from 'react';
+
+
+function App() {
+  const [profile, setProfile] = useState({ id: null, name: "" });
+
+  const handleProfile = (id, name) => {
+    setProfile({ id, name });
+  };
+
+  return (
+    <div className="App">
+      <div className="ui raised very text container">
+        <div className="ui grid">
+          <div className="eight wide column">
+            <List handleProfile={handleProfile} />
+          </div>
+          <div className="eight wide column">
+            {profile ? <Details info={profile} /> : null}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
